eb836cceea762a5d33d29b5c5fd7800a4fe26e11
istareatscreens <schar138@uottawa.ca>
2020-11-30 05:58:43 -0500

    Configured canvas in javascript, added styles, fixed html pathing

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //Draw canvas
-body = document.getElementsByTagName("body")[0];
-canvas = document.createElement("canvas");
-body.appendChild(canvas);
-canvas.setAttribute("id", "canvas");
-canvas.setAttribute("height", window.innerHeight);
-canvas.setAttribute("width", window.innerWidth);
+
+function init() {
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
